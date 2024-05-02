b5cd68c1f40a2f4a36c8011cdc0f2f8afe96b5c1
Tomek <127766155+1337Tomek1337@users.noreply.github.com>
2024-05-02 13:14:54 +0200

    Added Limited & Unlimited Spammer

--- index.js
+++ index.js
@@ -23,9 +23,11 @@ log(centerText('Made in Poland', terminal));
 log(centerText('._.tomus._.', terminal));
 space();
 
-rl.question(text(centerText('Enter Webhook URL >> ', terminal)), (webhook) => {
+rl.question(text(centerText('Enter Webhook URL >> ', terminal)), async (webhook) => {
+    let hook;
+
     try {
-        const hook = new WebhookClient({
+        hook = new WebhookClient({
             url: webhook,
         });
     } catch (err) {
@@ -35,7 +37,71 @@ rl.question(text(centerText('Enter Webhook URL >> ', terminal)), (webhook) => {
         return;
     }
 
-    console.log('Works Perfect');
+    space();
+
+    rl.question(text(centerText('Enter message that you want to spam with >> ', terminal)), async (message) => {
+        msg = message;
+
+        if (!message) {
+            space();
+            log(centerText('It can\'t be empty!', terminal));
+            process.exit();
+            return;
+        }
+
+        space();
+
+        rl.question(text(centerText('Enter how many messages you want to send (leave if you want to spam unlimited times) >> ', terminal)), async (number) => {
+            if (number) {
+                if (isNaN(number)) {
+                    space();
+                    log(centerText('It must be a number!', terminal));
+                    process.exit();
+                    return;
+                }
+
+                space();
+                log(centerText(`Started Spamming Webhook ${number} times...`, terminal));
+                space();
+
+                for (let i = 0; i < number; i++) {
+                    try {
+                        await hook.send({
+                            content: message,
+                            username: 'Webhook Spammer',
+                            avatarURL: 'https://cdn.discordapp.com/attachments/1198618155862478990/1235294843203485716/360_F_556291020_q2ieMiOCKYbtoLITrnt7qcSL1LJYyWrU.png?ex=6633d979&is=663287f9&hm=6cf9bbbe18a880687dad19aa6755d850f1ab34548383205dcd3730f5f977db27&',
+                        });
+
+                        log(centerText(`[${getCurrentTime()}] (${i + 1} / ${number}) Sent "${message}"`, terminal));
+                    } catch (err) {
+                        log(centerText(`Some error occured: ${err}`, terminal));
+                    }
+                }
+                
+                space();
+                log(centerText(`Successfully Spammed Webhook ${number} times with "${message}".`, terminal));
+                process.exit();
+            } else {
+                space();
+                log(centerText(`Started Spamming Webhook ∞ times...`, terminal));
+                space();
+
+                for (let i = 0; 0 < 1; i++) {
+                    try {
+                        await hook.send({
+                            content: message,
+                            username: 'Webhook Spammer',
+                            avatarURL: 'https://cdn.discordapp.com/attachments/1198618155862478990/1235294843203485716/360_F_556291020_q2ieMiOCKYbtoLITrnt7qcSL1LJYyWrU.png?ex=6633d979&is=663287f9&hm=6cf9bbbe18a880687dad19aa6755d850f1ab34548383205dcd3730f5f977db27&',
+                        });
+
+                        log(centerText(`[${getCurrentTime()}] (${i + 1}) Sent "${message}"`, terminal));
+                    } catch (err) {
+                        log(centerText(`Some error occured: ${err}`, terminal));
+                    }
+                }
+            }
+        });
+    });
 });
 
 // some functions
@@ -43,6 +109,16 @@ rl.question(text(centerText('Enter Webhook URL >> ', terminal)), (webhook) => {
 function centerText(text, terminalWidth) {
     const padding = (terminalWidth - text.length) / 2;
     return ' '.repeat(padding) + text;
+}
+
+function getCurrentTime() {
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
 }
 
 function log(text) {
